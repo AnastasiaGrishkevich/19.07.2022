5ad36fa0f88750cb8e83f6f3f40f411597fe7a33
ecoo169 <yurausenko93@gmail.com>
2022-08-20 21:51:40 +0700

    !

---
+++ main.py
@@ -0,0 +1,11 @@
+print('Как вас зовут?')
+name = input()  # считываем строку и кладём её в переменную name
+print('Здравствуйте, ' + name + '!')
+
+
+first = 5
+second = 7
+print(first * second)
+first = '5'
+second = '7'
+print(first * second)
